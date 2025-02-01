f5bf1b2105095c6c748d034d8bc3c7ca4eb6fa5e
aryan4859 <aryan.shrestha@presidnetial.edu.np>
2025-02-01 09:13:53 -0500

    added: loading component

---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,21 @@
+import { Grid } from "react-loader-spinner";
+
+const Loading = () => {
+    return (
+      <div className="h-[70vh] flex justify-center items-center">
+        <Grid
+          visible={true}
+          height="80"
+          width="80"
+          color="#EB5286"
+          ariaLabel="grid-loading"
+          radius="12.5"
+          wrapperStyle={{}}
+          wrapperClass="grid-wrapper"
+        />
+      </div>
+    );
+  };
+  
+  export default Loading;
+  
